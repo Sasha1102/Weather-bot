c08d3c7d32af3fe71377f1567c9f879dd4a528e5
Your Name <you@example.com>
2024-05-12 19:48:27 +0300

    testing some fiches

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import telebot
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, KeyboardButton, ReplyKeyboardMarkup
 from config import TOKEN
 from service import *
+import random
 
 bot = telebot.TeleBot(TOKEN)
 
@@ -14,10 +16,22 @@ def start_message(m):
     if create_user_if_not_exist(m.from_user):
         msg = bot.send_message(m.chat.id, "Вітаємо! Напишіть ваше місто")
         bot.register_next_step_handler(msg, handle_city)
-    if not get_city(m.from_user.id):
+    elif not get_city(m.from_user.id):
         msg = bot.send_message(m.chat.id, "Вітаємо! Напишіть ваше місто")
         bot.register_next_step_handler(msg, handle_city)
+    button2 = KeyboardButton(text="Share Location", request_location=True)
+    keyboard = ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+    # button1 = InlineKeyboardButton(text="CВ", url="https://google.com")
+    # keyboard.keyboard = [[button1]]
+    # bot.send_message(m.chat.id, "йцукен", reply_markup=keyboard)
+    keyboard.add(button2)
+    bot.send_message(m.chat.id, "йцукен2", reply_markup=keyboard)
 
+
+@bot.callback_query_handler(func=lambda call:True)
+def callback_inline(call):
+    if call.data == "SS":
+        pass
 
 if __name__ == '__main__':
     bot.polling()
